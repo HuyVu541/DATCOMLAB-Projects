0e48e4a7234969e2e9d997dbdfed0b9f58eb6332
Huy Vu <87607561+HuyVu541@users.noreply.github.com>
2024-07-30 16:38:58 +0700

    Add files via upload

---
+++ RNN + LSTM in VN30 Stock Prediction/RSI.py
@@ -0,0 +1,9 @@
+import pandas as pd
+def rsi(data, ticker):
+    rsi_period = 10
+    delta = data[ticker].diff()
+    gain = (delta.where(delta > 0, 0)).rolling(window=rsi_period).mean()
+    loss = (-delta.where(delta < 0, 0)).rolling(window=rsi_period).mean()
+    rs = gain / loss
+    rsi = 100 - (100 / (1 + rs))
+    return rsi
