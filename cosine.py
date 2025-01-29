eea33273034ea84ff5a5b4e745dfa1776783fdb2
Huy Vu <87607561+HuyVu541@users.noreply.github.com>
2025-01-29 20:10:11 +0700

    Deleted unnecessary files

--- cosine.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import numpy as np
-from numpy.linalg import norm
- 
-def cosine(window1, window2, window):
-    window1 = window1.to_numpy()
-    window2 = window2.to_numpy()
-    cosine = np.dot(window1, window2)/(norm(window1)*norm(window2))
-    return cosine
